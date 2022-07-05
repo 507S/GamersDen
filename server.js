61b646bf10d8843adfd5c5c2f7f17877d895c94d
unknown <ahnafakib@iut-dhaka.edu>
2022-07-05 15:22:53 +0600

    completed error handler

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 require('dotenv').config({path: "./config.env"})
 const express = require('express')
 const connectDB = require('./config/db')
-
+const errorHandler= require('./middleware/error')
 //connect db
 
 connectDB();
@@ -11,6 +11,9 @@ const app = express()
 app.use(express.json()) // help us to get data from the body -- middleware
 
 app.use('/api/auth', require("./routes/auth"))
+
+//error handles  should be last piece of middleware
+app.use(errorHandler)
 
 const PORT = process.env.PORT ||5000;
 
